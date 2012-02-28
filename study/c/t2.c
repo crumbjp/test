ef345ce5ecb4874395503d58259e4d1be1ff65a6
Hiroaki Kubota <cat.s.panch@gmail.com>
2012-02-28 19:11:09 +0900

    C

---
+++ study/c/t2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func1 ( int arg ) {
+  printf("%d\n",arg);
+  return 0;
+}
+
---
+++ study/c/t3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int func2 ( char *arg ) {
+  printf("%s\n",arg);
+  return 0;
+}
+
