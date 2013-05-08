7150178bd9668f915c8f24c94599f6546c18e5d7
crumbjp <hiroaki.kubota@mail.crumb.jp>
2013-05-08 19:39:44 +0900

    test script

--- study/mongo/small.pl
+++ study/mongo/small.pl
@@ -8,15 +8,15 @@ for ( my $f = 0 ; $f <= 9 ; $f++ ) {
     for ( my $i = $f*$NUM +1 ; $i <= $f*$NUM + $NUM; $i++ ) {
 	my $LINE = '{' .
 	    '"_id":'.$i.','.
-	    '"value1":"aaaaaaaaaaaaaaaaaaaa",'.
+	    '"value1":"aaaaaaaaaa",'.
 	    '"value2":"bbbbbbbbbbbbbbbbbbbb",'.
-	    '"value3":"cccccccccccccccccccc",'.
-	    '"value4":"dddddddddddddddddddd",'.
-	    '"value5":'.$i % 4000 . ','.
-	    '"value6":"eeeeeeeeeeeeeeeeeeee",'.
-	    '"value7":"ffffffffffffffffffff",'.
-	    '"value8":"gggggggggggggggggggg",'.
-	    '"value9":"hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",'.
+	    '"value3":"cccccccccccccccccccccccccccccc",'.
+	    '"value4":"dddddddddddddddddddddddddddddddddddddddddddddddddd",'.
+	    '"value5":'.$i % 40000 . ','.
+	    '"value6":"eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",'.
+	    '"value7":"",'.
+	    '"value8":"",'.
+	    '"value9":"hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",'.
 	    '"value0":'.$i.'}'."\n";
 	print $FP $LINE;
 	
